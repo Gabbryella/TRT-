00fb794120a0489e042f6adde542ea459aaafb75
Gabbryella <gabriela.nshombo@gmail.com>
2024-10-22 14:30:56 +0100

    ttt

---
+++ src/apiConfig.js
@@ -0,0 +1,9 @@
+const API_BASE_URL = 'https://ec26-102-68-63-214.ngrok-free.app/api';
+
+const config = {
+    headers: {
+        'ngrok-skip-browser-warning': 'true'
+    }
+};
+
+export { config, API_BASE_URL };
